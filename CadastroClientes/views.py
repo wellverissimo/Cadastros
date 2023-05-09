a42dda93de30efef0b3df687ac2b43d3e06fd265
WELLINGTON_SILVA <wellington_silva@hotmail.com>
2023-05-08 22:28:37 -0300

    incluindo url,view e template

--- CadastroClientes/views.py
+++ CadastroClientes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    meu_nome = 'Wellington Verissimo'
+    lista_frutas = ['Laranja', 'Maçã','Banana']
+    context = {
+        'nome': meu_nome,
+        'frutas' : lista_frutas
+    }
+    return render(request, 'index.html', context)
