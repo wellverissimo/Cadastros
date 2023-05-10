c0c1d8d6257c07bd821eeb3876aadc0a9ca5abb9
WELLINGTON_SILVA <wellington_silva@hotmail.com>
2023-05-10 20:24:43 -0300

    Adicionando o /clientes no site

--- CadastroClientes/urls.py
+++ CadastroClientes/urls.py
@@ -3,4 +3,5 @@ from CadastroClientes import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('clientes', views.listar_clientes, name='clientes')
 ]
--- CadastroClientes/views.py
+++ CadastroClientes/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from CadastroClientes.models import Cliente
 
 # Create your views here.
 def index(request):
     meu_nome = 'Wellington Verissimo'
-    lista_frutas = ['Laranja', 'Maçã','Banana']
+    lista_frutas = ['Laranja', 'Maçã','Banana','Uva','Pera']
     context = {
         'nome': meu_nome,
         'frutas' : lista_frutas
     }
     return render(request, 'index.html', context)
+
+def listar_clientes(request):
+    # busca todos os clientes cadastrados na tabela(admin)
+    lista_clientes = Cliente.objects.all()
+    # o dicionário (variável) context é que vai mandar pro template
+    context = {
+        "clientes": lista_clientes,
+    }
+
+
+    return render(request, 'lista_clientes.html', context)
